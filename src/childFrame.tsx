67a269c1f9d4f1b3b36bc3d0f9b8221313402a9c
LeviHHH <LeviHHH@protonmail.com>
2022-10-28 20:04:53 +0800

    demo wallet

--- src/childFrame.tsx
+++ src/childFrame.tsx
@@ -1,6 +1,20 @@
+import { BCS, TxnBuilderTypes } from "aptos";
 import { useEffect, useState } from "react";
 import { MsafeWallet } from "./iframe/MsafeWallet";
+import { Buffer } from "buffer";
 
+const fakePayload = new TxnBuilderTypes.TransactionPayloadEntryFunction(
+    TxnBuilderTypes.EntryFunction.natural("0x1::coin", "transfer", [], [])
+);
+const fakeTxn = new TxnBuilderTypes.RawTransaction(
+    TxnBuilderTypes.AccountAddress.fromHex("0x123"),
+    BigInt(0),
+    fakePayload,
+    BigInt(10000),
+    BigInt(100),
+    BigInt(1982241224),
+    new TxnBuilderTypes.ChainId(31)
+);
 const msafeUrl = "http://localhost:3000";
 function ChildIFrame() {
     const [wallet, setWallet] = useState<MsafeWallet>();
@@ -9,40 +23,108 @@ function ChildIFrame() {
     const [notification, setNotification] = useState<string>();
 
     // child connect to msafe
-    async function connect() {
+    async function handshake() {
         if (wallet) return;
         const w = await MsafeWallet.new();
-        w.onChangeAccount((account)=>setNotification(JSON.stringify(account)));
-        w.onChangeNetwork((network)=>{
-            setNotification(network)
+        w.onChangeAccount((account) =>
+            setNotification(JSON.stringify(account))
+        );
+        w.onChangeNetwork((network) => {
+            setNotification(network);
         });
         setWallet(w);
     }
-    async function signTransaction(tx: string) {
+    async function connect() {
         if (wallet) {
             try {
-                const response = await wallet.signTransaction(Buffer.from(tx));
-                setResponse(Buffer.from(response).toString());
+                const account = await wallet.connect();
+                setResponse(`address:${account.address}\npublic key:${account.publicKey}`);
                 setError(undefined);
             } catch (e: any) {
                 setResponse(undefined);
-                setError(Buffer.from(e.message).toString());
+                setError(e.message);
+            }
+        }
+    }
+    async function disconnect() {
+        await wallet?.disconnect();
+    }
+    async function network() {
+        const network = await wallet?.network();
+        setResponse(network);
+        setError(undefined);
+    }
+    async function account() {
+        const account = await wallet?.connect();
+        setResponse(`address:${account!.address}\npublic key:${account!.publicKey}`);
+        setError(undefined);
+    }
+    async function signAndSubmit() {
+        if (wallet) {
+            try {
+                const response = await wallet.signAndSubmit(
+                    BCS.bcsToBytes(fakeTxn)
+                );
+                setResponse(Buffer.from(response).toString("hex"));
+                setError(undefined);
+            } catch (e: any) {
+                setResponse(undefined);
+                setError(e.message);
+            }
+        }
+    }
+    async function signMessage() {
+        if (wallet) {
+            try {
+                const sig = await wallet.signMessage("hello");
+                setResponse(Buffer.from(sig).toString("hex"));
+                setError(undefined);
+            } catch (e: any) {
+                setResponse(undefined);
+                setError(e.message);
+            }
+        }
+    }
+    async function signTransaction() {
+        if (wallet) {
+            try {
+                const response = await wallet.signTransaction(
+                    BCS.bcsToBytes(fakeTxn)
+                );
+                setResponse(Buffer.from(response).toString("hex"));
+                setError(undefined);
+            } catch (e: any) {
+                setResponse(undefined);
+                setError(e.message);
+            }
+        }
+    }
+    async function chainId() {
+        if (wallet) {
+            try {
+                const response = await wallet.chainId();
+                setResponse(response.toString());
+                setError(undefined);
+            } catch (e: any) {
+                setResponse(undefined);
+                setError(e.message);
             }
         }
     }
     useEffect(() => {
-        connect();
-    }, [wallet]);
+        handshake();
+    }, [wallet, handshake]);
     return (
         <>
             <p>===================child frame: {window.location.href}</p>
-            <button onClick={() => connect()}>handshake</button>
-            <button onClick={() => signTransaction(window.location.href)}>
-                signTransaction
-            </button>
-            <button onClick={() => signTransaction("tx-invalid")}>
-                signTransaction Error
-            </button>
+            <button onClick={() => connect()}>connect</button>
+            <button onClick={() => disconnect()}>disconnect</button>
+            <button onClick={() => network()}>network</button>
+            <button onClick={() => account()}>account</button>
+            <button onClick={() => chainId()}>chainId</button>
+            <button onClick={() => signAndSubmit()}>signAndSubmit</button>
+            <button onClick={() => signTransaction()}>signTransaction</button>
+            <button onClick={() => signMessage()}>signMessage</button>
             <p>
                 handshake:
                 {wallet && wallet.client.connector.connected
--- src/iframe.tsx
+++ src/iframe.tsx
@@ -1,29 +1,115 @@
+import {
+    BCS,
+    TransactionBuilderRemoteABI,
+    TxnBuilderTypes,
+    AptosClient,
+    TransactionBuilder,
+    HexString,
+    AptosAccount,
+} from "aptos";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { Connector } from "./iframe/connector";
-import { MsafeServer, Option } from "./iframe/MsafeServer";
-import { Payload } from "./iframe/payload";
+import { MsafeServer } from "./iframe/MsafeServer";
+import { Payload, Option, Account } from "./iframe/WalletAPI";
 
+const aptosClient = new AptosClient(
+    "https://fullnode.testnet.aptoslabs.com/v1"
+);
 const dappUrl = "http://127.0.0.1:3001";
-function IFrame() {
+
+async function buildTransaction(
+    payload: Payload,
+    option?: Option
+): Promise<TxnBuilderTypes.RawTransaction> {
+    if (payload instanceof Uint8Array) {
+        const deserializer = new BCS.Deserializer(payload);
+        return TxnBuilderTypes.RawTransaction.deserialize(deserializer);
+    }
+    const txnBuilder = new TransactionBuilderRemoteABI(aptosClient, option!);
+    const moduleId = TxnBuilderTypes.ModuleId.fromStr(payload.function);
+    return txnBuilder.build(
+        moduleId.name.value,
+        payload.type_arguments,
+        payload.arguments
+    );
+}
+
+export function IFrame() {
     const iframeRef = useRef(null);
     const [enIframe, setEnIframe] = useState(false);
     const [msafe, setMsafe] = useState<MsafeServer>();
     const [request, setRequest] = useState<string>();
- 
+
     const accept = useCallback(() => {
         const cleaner = Connector.accepts(dappUrl, (connector) => {
-            setMsafe(new MsafeServer(connector, {
-                async signTransaction(payload:Payload, option:Option):Promise<Uint8Array> {
-                    if(payload instanceof Uint8Array) {
-                        if(Buffer.from(payload).toString().includes('invalid')) throw Error(`error-tx`);
-                    }
-                    setRequest('signTransaction');
-                    return Buffer.from("signed");
-                }
-            }));
+            const webWallet = (window as any).martian;
+            setMsafe(
+                new MsafeServer(connector, {
+                    async connect(): Promise<Account> {
+                        setRequest("connect");
+                        // should return msafe address and public key;
+                        return webWallet.connect();
+                    },
+                    async disconnect(): Promise<void> {
+                        setRequest("disconnect");
+                        return webWallet.disconnect();
+                    },
+                    async network(): Promise<string> {
+                        setRequest("network");
+                        return "Testnet";
+                    },
+                    async account(): Promise<Account> {
+                        setRequest("account");
+                        // should return msafe address and public key;
+                        return webWallet.account();
+                    },
+                    async chainId(): Promise<Number> {
+                        setRequest("chainId");
+                        return webWallet
+                            .getChainId()
+                            .then((r: any) => r.chainId);
+                    },
+                    async signAndSubmit(
+                        payload: Payload,
+                        option?: Option
+                    ): Promise<Uint8Array> {
+                        setRequest("signAndSubmit");
+                        const txn = await buildTransaction(payload, option);
+                        console.log("signAndSubmit:", BCS.bcsToBytes(txn));
+                        // msafe.init_transaction(txn);
+                        // msafe.submit_signature();
+                        // txhash = clien.submitBCSTranscion(...);
+                        // ...
+                        const fakeTxHash = new Uint8Array(32);
+                        fakeTxHash.set(
+                            BCS.bcsToBytes(txn.payload).subarray(0, 32)
+                        );
+                        return fakeTxHash;
+                    },
+                    async signTransaction(
+                        payload: Payload,
+                        option?: Option
+                    ): Promise<Uint8Array> {
+                        setRequest("signTransaction");
+                        const txn = await buildTransaction(payload, option);
+                        console.log("signTransaction:", BCS.bcsToBytes(txn));
+                        // msafe.init_transaction(txn);
+                        // msafe.submit_signature();
+                        // multiTxn = 
+                        const fakeSignedTxn = txn;
+                        return BCS.bcsToBytes(fakeSignedTxn);
+                    },
+                    async signMessage(
+                        message: string | Uint8Array
+                    ): Promise<Uint8Array> {
+                        setRequest("signMessage");
+                        throw Error("unsupport");
+                    },
+                })
+            );
         });
         return cleaner;
-    },[]);
+    }, []);
     // msafe accpet connection
     useEffect(() => {
         const cleaner = accept();
@@ -31,16 +117,21 @@ function IFrame() {
             cleaner();
             msafe && msafe.server.connector.close();
         };
-    },[accept, msafe]);
+    }, [accept, msafe]);
     return (
         <>
             <p>===================msafe frame: {window.location.href}</p>
             <button onClick={() => setEnIframe(true)}>load iframe</button>
-            <button onClick={() => msafe && msafe.changeNetwork(window.location.href)}>
+            <button
+                onClick={() =>
+                    msafe && msafe.changeNetwork(window.location.href)
+                }
+            >
                 notify
             </button>
             <p>
-                handshake:{msafe && msafe.server.connector.connected
+                handshake:
+                {msafe && msafe.server.connector.connected
                     ? "connected"
                     : "disconnected"}
             </p>
@@ -51,7 +142,7 @@ function IFrame() {
                     title="Dapp"
                     width="100%"
                     height="400hv"
-                    src={dappUrl+'/child'}
+                    src={dappUrl + "/child"}
                 />
             )}
         </>
